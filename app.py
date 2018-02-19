24fe50025e4af0b5170a60e415105222ed5b245f
Junjun Zhang <junjun.ca@gmail.com>
2018-02-18 21:17:27 -0500

    added support for CORS

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import connexion
 import logging
+from flask_cors import CORS
 
 
 logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__)
 app.add_api('swagger/jt-jess.yaml', base_path='/api/jt-jess/v0.1')
+CORS(app.app)
 
 if __name__ == '__main__':
     # run our standalone gevent server
