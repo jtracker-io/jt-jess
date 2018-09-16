93eda007515940ac78b34a033cbc92df38b7099f
Junjun Zhang <junjun.ca@gmail.com>
2018-09-16 11:56:09 -0400

    bump up to 0.2.0a20 (sync with jt-cli)

--- jt_jess/__init__.py
+++ jt_jess/__init__.py
@@ -1,7 +1,7 @@
 from . import job, queue, executor as exe, task
 from .exceptions import OwnerNameNotFound, AMSNotAvailable, WorklowNotFound, WRSNotAvailable, QueueCreationFailure
 
-__version__ = '0.2.0a18'
+__version__ = '0.2.0a20'
 
 
 def get_jobs(owner_name, queue_id, job_id=None, state=None):
